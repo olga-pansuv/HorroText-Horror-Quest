f815623042464fc264f3c17adc7ca09bbd2e058d
Полина <linanaumenko1@gmail.com>
2026-01-09 08:46:33 +0300

    продолжение кода и json

--- Project/scr/game_start.py
+++ Project/scr/game_start.py
@@ -8,8 +8,25 @@ with open("story.json", "r", encoding="utf-8") as f:
 
 current_scene = "start"
 player_name = ""
+state = {
+    "refused_journalist": False,
+    "close_seat": False
+}
 
-def show_scene(scene_id):
+def get_choices(scene, mode="default"):
+    choices = scene.get("choices", [])
+
+    # если choices — словарь с вариантами
+    if isinstance(choices, dict) and "variants" in choices:
+        return choices["variants"].get(mode, [])
+
+    # если choices — обычный список
+    elif isinstance(choices, list):
+        return choices
+
+    return []
+
+def show_scene(scene_id, mode="default"):
     global current_scene
     current_scene = scene_id
     scene = story[scene_id]
@@ -20,22 +37,41 @@ def show_scene(scene_id):
         text = text.replace('{name}', player_name)
     text_box.insert(tk.END, text)
     text_box.config(state='disabled')
+
     for widget in buttons_frame.winfo_children(): # очистка кнопок
         widget.destroy()
 
-    for choice in scene.get("choices", []):
-        button = tk.Button(
+    choices = get_choices(scene, mode)
+    for choice in choices:
+        btn = tk.Button(
             buttons_frame,
             text=choice['text'],
             wraplength=400,
-            width=10,
+            width=15,
             command=lambda n=choice["next"]: on_choice(n)
         )
-        button.pack(fill='x', pady=5)
+        btn.pack(fill='x', pady=5)
+
+
+
 def on_choice(next_scene):
+    global player_name, state
+
     if next_scene == "introduction" and not player_name:
         ask_name()
-    show_scene(next_scene)
+
+    if next_scene == "refuse":
+        state["refused_journalist"] = True
+
+    if next_scene == "close_seat_v1":
+        state["close_seat"] = True
+
+    mode = "default"
+    if next_scene == "corridor_cross":
+        if state["refused_journalist"] and state["close_seat"]:
+            mode = "extended"
+    show_scene(next_scene, mode=mode)
+
 
 def ask_name():
     global player_name
